a6d660a88e2ee87ef17278e3db1d7dd66d9f2b7e
dskrenta <dskrenta@gmail.com>
2017-04-28 09:48:18 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './App.css';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Colts Hack</h1>
+      </div>
+    );
+  }
+}
