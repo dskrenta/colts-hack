db353a218e62fdd51195942e9f3def901eac0223
dskrenta <dskrenta@gmail.com>
2017-04-28 12:39:31 -0700

    various changes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 'use strict';
 const Koa = require('koa');
+const convert = require('koa-convert');
+const cors = require('koa-cors');
 const getMarketData = require('./get-data');
+const Backtest = require('./backtest');
 const app = new Koa();
+
+app.use(convert(cors()));
 
 app.use(async (ctx) => {
   switch(ctx.request.url) {
@@ -10,7 +15,10 @@ app.use(async (ctx) => {
       ctx.body = 'base page'
     case '/api':
       ctx.type = 'application/json';
-      ctx.body = await getMarketData();
+      // ctx.body = await getMarketData();
+      const marketData = await getMarketData();
+      const bt = new Backtest(marketData, 5, 0.90, ['SAR']);
+      ctx.body = await bt.main();
       break;
     default:
       ctx.type = 'text/html';
---
+++ src/Backtest.js
@@ -0,0 +1,103 @@
+export default class Backtest {
+  constructor (
+    marketData,
+    numContracts = 5,
+    avgDelta = 0.90
+  ) {
+    this.marketData = marketData;
+    this.shares = avgDelta * 100 * numContracts;
+    this.orderBook = [];
+    this.currentPosition = {};
+    this.totalPL = 0;
+  }
+
+  get getCurrentLongPosition () {
+    return this.currentPosition.long;
+  }
+
+  get getCurrentShortPosition () {
+    return this.currentPosition.short;
+  }
+
+  setTradingLogic (tradingLogic) {
+    this.tradingLogic = tradingLogic;
+    this.main();
+  }
+
+  marketDataStartIndex () {
+    const begIndexes = this.taData.map(taObj => taObj.begIndex);
+    return Math.max(begIndexes);
+  }
+
+  openLong (bar) {
+    this.orderBook.push({type: 'Open Long', marketData: bar, price: bar.open});
+    this.currentPosition.long = {type: 'long', price: bar.open, shares: this.shares};
+  }
+
+  closeLong (bar) {
+    this.orderBook.push({type: 'Close Long', marketData: bar, price: bar.open});
+    // console.log('LONG: ' + (bar.open - this.currentPosition.long.price) * this.currentPosition.long.shares);
+    this.totalPL += (bar.open - this.currentPosition.long.price) * this.currentPosition.long.shares;
+    this.currentPosition.long = null;
+  }
+
+  openShort (bar) {
+    this.orderBook.push({type: 'Open Short', marketData: bar, price: bar.open});
+    this.currentPosition.short = {type: 'short', price: bar.open, shares: -1 * this.shares};
+  }
+
+  closeShort (bar) {
+    this.orderBook.push({type: 'Close Short', marketData: bar, price: bar.open});
+    // console.log('SHORT: ' + (bar.open - this.currentPosition.short.price) * this.currentPosition.short.shares);
+    this.totalPL += (bar.open - this.currentPosition.short.price) * this.currentPosition.short.shares;
+    this.currentPosition.short = null;
+  }
+
+  longStop (price) {
+    this.orderBook.push({type: 'Long Stop', price: price});
+    this.currentStop = {type: 'Long Stop', price: price};
+  }
+
+  shortStop (price) {
+    this.orderBook.push({type: 'Short Stop', price: price});
+    this.currentStop = {type: 'Short Stop', price: price};
+  }
+
+  clearStop () {
+    this.currentStop = null;
+  }
+
+  stopCheck (bar) {
+    if (this.currentStop) {
+      if (this.currentStop.type === 'Long Stop') {
+        if (bar.low <= this.currentStop.price) {
+          this.closeLong(bar);
+          this.currentStop = null;
+        }
+      } else {
+        if (bar.high >= this.currentStop.price) {
+          this.closeShort(bar);
+          this.currentStop = null;
+        }
+      }
+    }
+  }
+
+  backtestLoop () {
+    for (let bar of this.finalMarketData) {
+      this.tradingLogic(bar);
+      this.stopCheck(bar);
+    }
+  }
+
+  async main () {
+    try {
+      this.finalMarketData = this.marketData;
+      this.backtestLoop();
+
+      // console.log(`P/L: ${this.totalPL.toFixed(2)}`);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
